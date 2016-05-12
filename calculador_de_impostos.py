a881e3e84b87fc3619e3d59fc93bc08f3e3667d6
diegosaraujo <diesaraujo@gmail.com>
2016-05-12 10:02:14 -0300

    Utilizando o modulo e exibindo os resultados

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,8 +1,11 @@
-
+from impostos import calcula_ISS, calcula_ICMS
 class Calculador_de_impostos(object):
-      def realiza_calculo(self, orcamento):
-            imposto_calculado = orcamento.valor * 0.1
-            print (('O imposto calculado = %s') % (imposto_calculado) )
+      def realiza_calculo(self, orcamento, imposto):
+            if (imposto == 'ISS'):
+                  imposto_calculado = calcula_ISS(orcamento)
+            if (imposto == 'ICMS'):
+                  imposto_calculado = calcula_ICMS(orcamento)
+            print (('O valor do %s = %s') % (imposto, imposto_calculado))
 
 #O codigo abaixo esta sendo executado so para evitar de criar uma outra classe para isso
 #Basta executar este .py para obter o resultado. Caso importe este .py o codigo abaixo nao sera executado
@@ -13,4 +16,5 @@ if __name__ == '__main__':
 
       orcamento = Orcamento(500)
 
-      calculador.realiza_calculo(orcamento)
+      calculador.realiza_calculo(orcamento, 'ISS')
+      calculador.realiza_calculo(orcamento, 'ICMS')
