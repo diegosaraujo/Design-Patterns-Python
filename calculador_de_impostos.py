a87ca350bc42bc5221cc67e109d024513a86ca60
diegosaraujo <diesaraujo@gmail.com>
2016-05-12 10:34:44 -0300

    Aplicando o Strategy através da função

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,11 +1,11 @@
 from impostos import calcula_ISS, calcula_ICMS
 class Calculador_de_impostos(object):
+
       def realiza_calculo(self, orcamento, imposto):
-            if (imposto == 'ISS'):
-                  imposto_calculado = calcula_ISS(orcamento)
-            if (imposto == 'ICMS'):
-                  imposto_calculado = calcula_ICMS(orcamento)
-            print (('O valor do %s = %s') % (imposto, imposto_calculado))
+
+            imposto_calculado = imposto(orcamento)
+
+            print imposto_calculado
 
 #O codigo abaixo esta sendo executado so para evitar de criar uma outra classe para isso
 #Basta executar este .py para obter o resultado. Caso importe este .py o codigo abaixo nao sera executado
@@ -16,5 +16,5 @@ if __name__ == '__main__':
 
       orcamento = Orcamento(500)
 
-      calculador.realiza_calculo(orcamento, 'ISS')
-      calculador.realiza_calculo(orcamento, 'ICMS')
+      calculador.realiza_calculo(orcamento, calcula_ISS)
+      calculador.realiza_calculo(orcamento, calcula_ICMS)
