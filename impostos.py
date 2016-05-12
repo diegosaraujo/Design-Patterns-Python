bcf8aab1591f8d5e6377d8ced1a426602df6c9b0
diegosaraujo <diesaraujo@gmail.com>
2016-05-12 09:56:15 -0300

    Criada um modulo de impostos

---
+++ impostos.py
@@ -0,0 +1,8 @@
+#Modulo criado para ajudar a calcular varios tipos de impostos diferentes
+
+def calcula_ISS(orcamento):
+      return orcamento.valor * 0.1
+
+def calcula_ICMS(orcamento):
+      return orcamento.valor * 0.06
+
