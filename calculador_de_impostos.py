b86b2d306d4eab2c42f2226e10a954ccaae268ca
diegosaraujo <diesaraujo@gmail.com>
2016-05-12 09:49:28 -0300

    Criado o metodo e execução da calculadora

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -2,3 +2,15 @@
 class Calculador_de_impostos(object):
       def realiza_calculo(self, orcamento):
             imposto_calculado = orcamento.valor * 0.1
+            print (('O imposto calculado = %s') % (imposto_calculado) )
+
+#O codigo abaixo esta sendo executado so para evitar de criar uma outra classe para isso
+#Basta executar este .py para obter o resultado. Caso importe este .py o codigo abaixo nao sera executado
+if __name__ == '__main__':
+      from orcamento import Orcamento
+
+      calculador = Calculador_de_impostos()
+
+      orcamento = Orcamento(500)
+
+      calculador.realiza_calculo(orcamento)
